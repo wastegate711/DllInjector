87bc643d090252f9619ee4e748bc65d7a4c6450e
wastegate711 <evgeni.shvec@gmail.com>
2023-08-29 15:54:22 +0300

    Добавил контролы и к ним команды.

--- DllInjector/ViewModels/MainWindowViewModel.cs
+++ DllInjector/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,115 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Microsoft.Win32;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace DllInjector.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
         public MainWindowViewModel()
         {
 
         }
+
+        #region Свойства
+
+        private string _libraryPath = "";
+
+        /// <summary>
+        /// Содержит путь к библиотеке которую нужно инжектить.
+        /// </summary>
+        public string LibraryPath
+        {
+            get => _libraryPath;
+            set => SetProperty(ref _libraryPath, value);
+        }
+
+        private string _title = "DLL Injector";
+
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private ObservableCollection<Process> _listProcess;
+
+        /// <summary>
+        /// Список найденых процессов.
+        /// </summary>
+        public ObservableCollection<Process> ListProcess
+        {
+            get { return _listProcess; }
+            set { SetProperty(ref _listProcess, value); }
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region Открытие окна поиска библиотеки
+        private DelegateCommand _findLibrary;
+
+        /// <summary>
+        /// Открытие окна поиска библиотеки.
+        /// </summary>
+        public DelegateCommand FindLibrary            =>
+            _findLibrary ??= new DelegateCommand(ExecuteFindLibrary);
+
+        private void ExecuteFindLibrary()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter="(.dll)|*.dll";
+            ofd.Title="Find Library";
+
+            bool? result = ofd.ShowDialog();
+
+            if(result==true)
+            {
+                LibraryPath = ofd.FileName;
+            }
+        }
+        #endregion
+
+        #region Обновить список процессов
+
+        private DelegateCommand _refreshListProcess;
+
+        /// <summary>
+        /// Обновить список процессов
+        /// </summary>
+        public DelegateCommand RefreshListProcess =>
+            _refreshListProcess ??= new DelegateCommand(ExecuteRefreshListProcess);
+
+        private void ExecuteRefreshListProcess()
+        {
+
+        }
+
+        #endregion
+
+        #region Inject Dll
+
+        private DelegateCommand _injectDll;
+
+        /// <summary>
+        /// Inject Dll
+        /// </summary>
+        public DelegateCommand InjectDll =>
+            _injectDll ??= new DelegateCommand(ExecuteInjectDll);
+
+        private void ExecuteInjectDll()
+        {
+
+        }
+
+        #endregion
+        #endregion
     }
 }
